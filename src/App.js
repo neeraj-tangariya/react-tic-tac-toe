c97e3c71ea3190c42bbba25cb38aaf90c7713f11
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-10-13 10:30:07 +0530

    intial game structure setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Icons from "./components/Icons";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+
+const itemArr = new Array(9).fill("empty");
 
 function App() {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {};
+
+  const checkIsWinner = () => {};
+
+  const changeItem = (itemNumber) => {};
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Icons />
       </header>
     </div>
   );
