a3a0bc49cac5d1d0c8d0119744121426f9ac8d07
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-10-13 11:07:48 +0530

    game logic-1 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
 import Icons from "./components/Icons";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
 import { useState } from "react";
 
 const itemArr = new Array(9).fill("empty");
@@ -12,18 +13,60 @@ function App() {
   const [isCross, setIsCross] = useState(false);
   const [winMessage, setWinMessage] = useState("");
 
-  const reloadGame = () => {};
+  const reloadGame = () => {
+    setIsCross(false);
+    setWinMessage("");
+    itemArr.fill("empty", 0, 9);
+  };
 
   const checkIsWinner = () => {};
 
-  const changeItem = (itemNumber) => {};
+  const changeItem = (itemNumber) => {
+    if (winMessage) {
+      return toast(winMessage, { type: "success" });
+    }
+
+    if (itemArr[itemNumber] === "empty") {
+      itemArr[itemNumber] = isCross ? "cross" : "circle";
+      setIsCross(!isCross);
+    } else {
+      return toast("already filled", { type: "error" });
+    }
+
+    checkIsWinner();
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Icons />
-      </header>
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          { winMessage ? (
+            <div className="mb-2 mt-2">
+              <h1 className="text-primary text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color="success" block onClick={reloadGame}>
+                Reload the game
+              </Button>
+            </div>
+          ) : (
+            <h1 className="text-center text-warning">
+              {isCross ? "Cross" : "Circle"} turns
+            </h1>
+          ) }
+          <div className="grid">
+            {itemArr.map((item, index) => (
+              <Card>
+                <CardBody className="box">
+                  <Icons name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
