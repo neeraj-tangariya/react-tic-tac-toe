6c072e8fccb590102507e9d0fa7f9103454dbb57
Neeraj1005 <neeraj.tangariya123@gmail.com>
2022-10-13 13:54:52 +0530

    app part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { Container } from "reactstrap";
+import { TodoContext } from "./context/TodoContext";
+import todoReducer from "./context/reducer";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  const [todos, dispatch] = useReducer(todoReducer, []);
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <TodoContext.Provider value={{todos, dispatch}}>
+      <Container fluid>
+        <h1>Todo app with context api</h1>
+      </Container>
+    </TodoContext.Provider>
   );
 }
 
---
+++ src/context/action.types.js
@@ -0,0 +1,2 @@
+export const ADD_TODO = "ADD_TOOD";
+export const REMOVE_TODO = "REMOVE_TOOD";
